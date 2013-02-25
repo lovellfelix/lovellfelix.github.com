0ed26694aa4c82b0efca1b3bc41d192b1a8b99d6
Lovell Felix <lovell.felix@gmail.com>
2013-02-24 23:06:44 -0600

    Migrated to Angularjs with new ui

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var githubApp = angular.module('githubApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
